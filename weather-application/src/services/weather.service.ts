795225e454b07388a3c91e88904a8a7f2be3fef8
Robert Nițu <niturobert02@yahoo.com>
2023-04-04 22:36:52 +0300

    Added prettier

--- weather-application/src/services/weather.service.ts
+++ weather-application/src/services/weather.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { environment } from "../environments/environment";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getWeather(cityName: string) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${environment.WeatherApiMapKey}&units=metric`);
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${environment.WeatherApiMapKey}&units=metric`
+    );
   }
 }
