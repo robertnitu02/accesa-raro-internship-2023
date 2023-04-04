0207e9a674c19256bef21102201ad1513a379c0c
Robert Nițu <niturobert02@yahoo.com>
2023-04-04 20:55:55 +0300

    Implemented openWeatherMap API. Wip

--- weather-application/src/app/app.component.ts
+++ weather-application/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "../services/weather.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-application';
+  city: string = 'Pucioasa';
+  myWeather: any;
+  myWeatherStringify: any;
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.weatherService.getWeather(this.city).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.myWeather = res;
+        console.log(this.myWeather);
+      },
+      error: (error) => console.log(error.message),
+      complete: () => console.log('API Call Completed')
+    })
+  }
+
+  change(): void {
+    this.weatherService.getWeather(this.city).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.myWeather = res;
+        console.log(this.myWeather);
+        this.myWeatherStringify = JSON.stringify(res);
+      },
+      error: (error) => console.log(error.message),
+      complete: () => console.log('API Call Completed')
+    })
+  }
 }
---
+++ weather-application/src/services/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(cityName: string) {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${environment.WeatherApiMapKey}&units=metric`);
+  }
+}
